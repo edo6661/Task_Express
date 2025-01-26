4ca13a66eebdfae9715d3546b1ab0fb7e5a2f40c
edo6661 <muhammadridho5654@gmail.com>
2025-01-27 00:19:53 +0700

    feat: limiter, timeout db

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,22 @@ import hpp from "hpp";
 import morgan from "morgan";
 import logger from "./lib/logger";
 import taskRouter from "./routes/task_route";
+import rateLimit from "express-rate-limit";
+
 dotenv.config();
 const app = express();
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
+
 app.use(
   compression({
     filter: (req, res) => {
@@ -26,6 +36,7 @@ app.use(
       return compression.filter(req, res);
     },
     level: 6,
+    threshold: 10 * 1000,
   })
 );
 app.use(hpp());
--- src/lib/db/database.ts
+++ src/lib/db/database.ts
@@ -3,6 +3,9 @@ import { drizzle } from "drizzle-orm/node-postgres";
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 5000,
 });
 
 export const db = drizzle(pool);
