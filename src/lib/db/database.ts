19ec31e875a861d5b2d01f158751f4eca8280add
edo6661 <muhammadridho5654@gmail.com>
2025-01-27 00:07:15 +0700

    feat: local compose

--- src/lib/db/database.ts
+++ src/lib/db/database.ts
@@ -2,19 +2,16 @@ import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-  // ! LOCAL
-  // connectionString: process.env.DATABASE_URL,
-  // connectionTimeoutMillis: 5000,
-  // query_timeout: 5000,
-  // lock_timeout: 5000,
-  // statement_timeout: 5000,
-  // idle_in_transaction_session_timeout: 5000,
-  // idleTimeoutMillis: 5000,
-  // database: "mydb",
-  // host: "db",
-  // port: 5432,
-  // user: "postgres",
+  connectionTimeoutMillis: 5000,
+  query_timeout: 5000,
+  lock_timeout: 5000,
+  statement_timeout: 5000,
+  idle_in_transaction_session_timeout: 5000,
+  idleTimeoutMillis: 5000,
+  database: "mydb",
+  host: "db",
+  port: 5432,
+  user: "postgres",
 });
 
 export const db = drizzle(pool);
