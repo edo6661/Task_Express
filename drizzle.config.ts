a62b7c2bf74f3d65156c21fc1bd33f32d8cf7224
edo6661 <muhammadridho5654@gmail.com>
2025-01-27 00:02:13 +0700

    feat: comment local

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,16 +5,28 @@ export default defineConfig({
   schema: "./src/lib/db/schema.ts",
   out: "./drizzle",
   dbCredentials: {
-    host: process.env.DATABASE_HOST!,
-    port: +process.env.DATABASE_PORT!,
-    user: process.env.DATABASE_USER!,
-    password: process.env.DATABASE_PASSWORD!,
-    database: process.env.DATABASE_NAME!,
-    ssl: true,
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
+    ssl: true, // Aktifkan SSL
   },
   migrations: {
     table: "user_tasks",
     schema: "./src/lib/db/migrations",
   },
+  // ! local
+  // dialect: "postgresql",
+  // schema: "./src/lib/db/schema.ts",
+  // out: "./drizzle",
+  // dbCredentials: {
+  //   host: process.env.DATABASE_HOST!,
+  //   port: +process.env.DATABASE_PORT!,
+  //   user: process.env.DATABASE_USER!,
+  //   password: process.env.DATABASE_PASSWORD!,
+  //   database: process.env.DATABASE_NAME!,
+  //   ssl: true,
+  //   url: process.env.DATABASE_URL,
+  // },
+  // migrations: {
+  //   table: "user_tasks",
+  //   schema: "./src/lib/db/migrations",
+  // },
 });
