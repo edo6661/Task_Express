dfc06d83c9864ac50eca6a299ee9cc0ca54a4d7b
edo6661 <muhammadridho5654@gmail.com>
2025-01-27 00:08:10 +0700

    feat: clean up prod

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,27 +6,10 @@ export default defineConfig({
   out: "./drizzle",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
-    ssl: true, // Aktifkan SSL
+    ssl: true,
   },
   migrations: {
     table: "user_tasks",
     schema: "./src/lib/db/migrations",
   },
-  // ! local
-  // dialect: "postgresql",
-  // schema: "./src/lib/db/schema.ts",
-  // out: "./drizzle",
-  // dbCredentials: {
-  //   host: process.env.DATABASE_HOST!,
-  //   port: +process.env.DATABASE_PORT!,
-  //   user: process.env.DATABASE_USER!,
-  //   password: process.env.DATABASE_PASSWORD!,
-  //   database: process.env.DATABASE_NAME!,
-  //   ssl: true,
-  //   url: process.env.DATABASE_URL,
-  // },
-  // migrations: {
-  //   table: "user_tasks",
-  //   schema: "./src/lib/db/migrations",
-  // },
 });
