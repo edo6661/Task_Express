8f1278c7d23aefcdcb2d98ba0f4657a25aa78857
edo6661 <muhammadridho5654@gmail.com>
2025-01-27 15:40:47 +0700

    feat: self

--- src/routes/auth_route.ts
+++ src/routes/auth_route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { login, register } from "../controllers/auth_controller";
+import { login, register, self } from "../controllers/auth_controller";
 import { authMiddleware } from "../middlewares/auth_middleware";
 
 const authRouter = Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
+authRouter.get("/", authMiddleware, self);
 
 export default authRouter;
